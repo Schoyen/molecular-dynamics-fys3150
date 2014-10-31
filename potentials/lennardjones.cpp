9a914180f91725e3ac387232d17e84f442e0c2df
Schoyen <oyvindschoyen@gmail.com>
2014-10-31 14:04:01 +0100

    Finish lennardjones.cpp

--- potentials/lennardjones.cpp
+++ potentials/lennardjones.cpp
@@ -1,4 +1,5 @@
 #include "lennardjones.h"
+#include <cmath>
 
 LennardJones::LennardJones(double sigma, double epsilon) :
     m_sigma(sigma),
@@ -10,4 +11,18 @@ LennardJones::LennardJones(double sigma, double epsilon) :
 void LennardJones::calculateForces(System *system)
 {
     m_potentialEnergy = 0; // Remember to compute this in the loop
+    double distanceBetweenAtoms = 0;
+    double divisionOfSigmaAndDistance = 0;
+    vec3 tempForce = 0; // N3L
+    vec3 distance;
+    for (int i = 0; i < system->atoms().size(); i ++) {
+        for (int j = i + 1; j < system->atoms().size(); j++) {
+            distance = system->atoms[i] - system->atoms[j];
+            distanceBetweenAtoms = distance.length();
+            divisionOfSigmaAndDistance = m_sigma/distanceBetweenAtoms;
+            m_potentialEnergy += 4*m_epsilon(pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
+            // You were here!
+            tempForce = 
+        }
+    }
 }
