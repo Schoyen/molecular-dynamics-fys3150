5f62e965ac7898e52ddb31b78818a66e8c813c37
Schoyen <oyvindschoyen@gmail.com>
2014-11-12 19:22:31 +0100

    FUCKINGFORCE

--- potentials/lennardjones.cpp
+++ potentials/lennardjones.cpp
@@ -34,37 +34,41 @@ void LennardJones::calculateForces(System *system)
     // TODO: You must check whether or not the atoms in neighbouring cells are within r_cut.
     
     CellList *celllist = system->celllist();
-    //std::cout << celllist->listOfCells()[0]->position << "\n" << std::endl;
-    /*
-    std::cout << celllist->listOfCells()[63]->position << std::endl;
-    temp = celllist->listOfCells()[0]->position - celllist->listOfCells()[63]->position;
-    x = temp.x();
-    y = temp.y();
-    z = temp.z();
-    std::cout << temp << std::endl;
-
-    // Minimum image criterion.
-    if (x > xlen * 0.5) x = x - xlen;
-    else if (x < -xlen * 0.5) x = x + xlen;
-    if (y > ylen * 0.5) y = y - ylen;
-    else if (y < -ylen * 0.5) y = y + ylen;
-    if (z > zlen * 0.5) z = z - zlen;
-    else if (z < -zlen * 0.5) z = z + zlen;
-    temp = vec3(x, y, z);
-    std::cout << temp << std::endl;
-    std::cout << temp.lengthSquared() << std::endl;
-    std::cout << 3 * pow(celllist->getrcut(), 2) << std::endl;
-    */
     for (int i = 0; i < (int) celllist->listOfCells().size(); i++) {
+
+        // Calculation of force inside each cell.
+        // This seems to be working.
+        if (!celllist->listOfCells()[i]->calculatedLocally) {
+            for (int k = 0; k < (int) celllist->listOfCells()[i]->atomsClose().size(); i++) {
+                for (int m = k + 1; m < (int) celllist->listOfCells()[i]->atomsClose().size(); m++) {
+                    distance = celllist->listOfCells()[i]->atomsClose()[k]->position - celllist->listOfCells()[i]->atomsClose()[m]->position;
+                    distanceBetweenAtoms = distance.length();
+                    divisionOfSigmaAndDistance = m_sigma/distanceBetweenAtoms;
+
+                    // Calculate the new potential energy.
+                    // This one needs to be implemented properly.
+                    m_potentialEnergy += 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
+                    divisionOfSigmaAndDistance = m_sigma/celllist->getrcut();
+                    m_potentialEnergy -= 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
+
+                    expressionOfForce = 4 * m_epsilon * (12 * pow(m_sigma, 12)/pow(distanceBetweenAtoms, 14) - 6 * pow(m_sigma, 6)/pow(distanceBetweenAtoms, 8));
+                    tempForce = distance * expressionOfForce;
+                    celllist->listOfCells()[i]->atomsClose()[k]->force.add(tempForce);
+                    tempForce = tempForce * (-1); // N3L
+                    celllist->listOfCells()[i]->atomsClose()[m]->force.add(tempForce);
+                }
+                celllist->listOfCells()[i]->calculatedLocally = true;
+            }
+        }
         for (int j = 0; j < (int) celllist->listOfCells().size(); j++) {
             if (i != j) {
                 temp = celllist->listOfCells()[i]->position - celllist->listOfCells()[j]->position;
                 x = temp.x();
                 y = temp.y();
                 z = temp.z();
-                //std::cout << temp << std::endl;
 
-                // Minimum image criterion.
+                // Minimum image criterion for the cells.
+                // This is needed for calculation of cells the are along the edges.
                 if (x > xlen * 0.5) x = x - xlen;
                 else if (x < -xlen * 0.5) x = x + xlen;
                 if (y > ylen * 0.5) y = y - ylen;
@@ -72,88 +76,41 @@ void LennardJones::calculateForces(System *system)
                 if (z > zlen * 0.5) z = z - zlen;
                 else if (z < -zlen * 0.5) z = z + zlen;
                 temp = vec3(x, y, z);
-                //std::cout << temp << std::endl;
-                // Some cells are not computed.
+
                 if (temp.lengthSquared() <= 3 * pow(celllist->getrcut(), 2)) {
-                    /*
-                    if (i == 0) {
-                        std::cout << celllist->listOfCells()[j]->position << std::endl;
-                    }
-                    */
                     for (int k = 0; k < (int) celllist->listOfCells()[i]->atomsClose().size(); k++) {
-                        // Calculation of force inside each cell.
-                        // Avoid calculating the force in each cell locally several times.
-                        // Make sure all cells are computed.
-                        if (!celllist->listOfCells()[i]->calculatedLocally) {
-                            std::cout << i << "\t" << "YAY" << std::endl;
-                            for (int m = k + 1; m < (int) celllist->listOfCells()[i]->atomsClose().size(); m++) {
-                                distance = celllist->listOfCells()[i]->atomsClose()[k]->position - celllist->listOfCells()[i]->atomsClose()[m]->position;
-    
-                                // Needed for minimum image criterion.
-                                //x = distance.x();
-                                //y = distance.y();
-                                //z = distance.z();
-
-                                // Do I need the minimum image criterion for the atoms locally?
-                                /*
-                                // Minimum image criterion.
-                                if (x > xlen * 0.5) x = x - xlen;
-                                else if (x < -xlen * 0.5) x = x + xlen;
-                                if (y > ylen * 0.5) y = y - ylen;
-                                else if (y < -ylen * 0.5) y = y + ylen;
-                                if (z > zlen * 0.5) z = z - zlen;
-                                else if (z < -zlen * 0.5) z = z + zlen;
-                                */
-                        
-                                // Needed for minimum image criterion.
-                                //distance = vec3(x, y, z);
-                        
-                                distanceBetweenAtoms = distance.length();
-                                divisionOfSigmaAndDistance = m_sigma/distanceBetweenAtoms;
-                                // Calculate the new potential energy.
-                                // This one needs to be implemented properly.
-                                m_potentialEnergy += 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
-                                divisionOfSigmaAndDistance = m_sigma/celllist->getrcut();
-                                m_potentialEnergy -= 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
-                                expressionOfForce = 4 * m_epsilon * (12 * pow(m_sigma, 12)/pow(distanceBetweenAtoms, 14) - 6 * pow(m_sigma, 6)/pow(distanceBetweenAtoms, 8));
-                                tempForce = distance * expressionOfForce;
-                                celllist->listOfCells()[i]->atomsClose()[k]->force.add(tempForce);
-                                tempForce = tempForce * (-1);
-                                celllist->listOfCells()[i]->atomsClose()[m]->force.add(tempForce);
-                            }
-                            celllist->listOfCells()[i]->calculatedLocally = true;
-                        }
-
-                        // Check whether of not he cells are within r_cut, at least for the potential.
+                        // Check whether of not the cells are within r_cut, at least for the potential.
                         // Calculation of force between each atom in neighbour cell[i] and cell[j].
                         for (int m = 0; m < (int) celllist->listOfCells()[j]->atomsClose().size(); m++) {
                             distance = celllist->listOfCells()[i]->atomsClose()[k]->position - celllist->listOfCells()[j]->atomsClose()[m]->position;
-                            if (distance.lengthSquared() <= pow(celllist->getrcut(), 2)) {
-                                x = distance.x();
-                                y = distance.y();
-                                z = distance.z();
+                            x = distance.x();
+                            y = distance.y();
+                            z = distance.z();
 
-                                // Minimum image criterion.
-                                if (x > xlen * 0.5) x = x - xlen;
-                                else if (x < -xlen * 0.5) x = x + xlen;
-                                if (y > ylen * 0.5) y = y - ylen;
-                                else if (y < -ylen * 0.5) y = y + ylen;
-                                if (z > zlen * 0.5) z = z - zlen;
-                                else if (z < -zlen * 0.5) z = z + zlen;
+                            // Minimum image criterion.
+                            if (x > xlen * 0.5) x = x - xlen;
+                            else if (x < -xlen * 0.5) x = x + xlen;
+                            if (y > ylen * 0.5) y = y - ylen;
+                            else if (y < -ylen * 0.5) y = y + ylen;
+                            if (z > zlen * 0.5) z = z - zlen;
+                            else if (z < -zlen * 0.5) z = z + zlen;
                         
-                                distance = vec3(x, y, z);
-                                distanceBetweenAtoms = distance.length();
-                                divisionOfSigmaAndDistance = m_sigma/distanceBetweenAtoms;
+                            distance = vec3(x, y, z);
+                            distanceBetweenAtoms = distance.length();
+                            divisionOfSigmaAndDistance = m_sigma/distanceBetweenAtoms;
 
+                            // Calculating the potential according to the formula.
+                            if (distance.lengthSquared() <= 3 * pow(celllist->getrcut(), 2)) {
                                 m_potentialEnergy += 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
                                 divisionOfSigmaAndDistance = m_sigma/celllist->getrcut();
                                 m_potentialEnergy -= 4 * m_epsilon * (pow(divisionOfSigmaAndDistance, 12) - pow(divisionOfSigmaAndDistance, 6));
-                                expressionOfForce = 4 * m_epsilon * (12 * pow(m_sigma, 12)/pow(distanceBetweenAtoms, 14) - 6 * pow(m_sigma, 6)/pow(distanceBetweenAtoms, 8));
-                                tempForce = distance * expressionOfForce;
-                                celllist->listOfCells()[i]->atomsClose()[k]->force.add(tempForce);
-                                tempForce = tempForce * (-1);
-                                celllist->listOfCells()[j]->atomsClose()[m]->force.add(tempForce);
                             }
+
+                            expressionOfForce = 4 * m_epsilon * (12 * pow(m_sigma, 12)/pow(distanceBetweenAtoms, 14) - 6 * pow(m_sigma, 6)/pow(distanceBetweenAtoms, 8));
+                            tempForce = distance * expressionOfForce;
+                            celllist->listOfCells()[i]->atomsClose()[k]->force.add(tempForce);
+                            tempForce = tempForce * (-1);
+                            celllist->listOfCells()[j]->atomsClose()[m]->force.add(tempForce);
                         }
                         m_kineticEnergy += 0.5 * celllist->listOfCells()[i]->atomsClose()[k]->mass() * celllist->listOfCells()[i]->atomsClose()[k]->velocity.lengthSquared();
                     }
@@ -163,11 +120,11 @@ void LennardJones::calculateForces(System *system)
     }
     m_temperature = (2.0/3.0) * (m_kineticEnergy/((double) system->atoms().size() * 1));
 
-    // TODO: Remember to compute temperature. The temperature is wrong.
     system->celllist()->emptyCells();
     system->celllist()->calculateCellAtoms();
 
     /*
+     * Needed for timing of the methods.
     // Old force calculation.
     for (int i = 0; i < (int) system->atoms().size(); i++) {
         for (int j = i + 1; j < (int) system->atoms().size(); j++) {
